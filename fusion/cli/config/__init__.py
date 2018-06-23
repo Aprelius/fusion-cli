ca03c54c0d69980ef943e3aedea2e8f921292786
Daniel Weiner <danny@phantomnet.net>
2018-06-22 23:57:48 -0700

    Added a hard-coded config module

---
+++ fusion/cli/config/__init__.py
@@ -0,0 +1,33 @@
+def DefaultArchitecture():
+    return 'x86_64'
+
+
+def DefaultVariant():
+    return 'Debug'
+
+
+def GetBuildFolder():
+    return 'build'
+
+
+def GetInstallFolder():
+    return 'install'
+
+
+def GetProjectFolder():
+    return 'projects'
+
+
+def GetSupportedArchitectures():
+    return {
+        'i386': ['i386'],
+        'x86_64': ['x86_64', 'AMD64']
+    }
+
+
+def GetSupportedVariants():
+    return ['Debug', 'Release']
+
+
+def GetSupportedVisualStudioVersions():
+    return ['vs2013', 'vs2015', 'vs2017']
